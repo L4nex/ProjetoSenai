6f2231f4832b2fe91665096a3b3dfcad40f44493
L4nex <mchadolucas@gmail.com>
2020-02-26 17:38:02 -0300

    Adicionado valores para o LocalStorage

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
+localStorage.setItem("key_da_propriedade", "Valor armazenado");
+
+const minha_propriedade = localStorage.getItem('key_da_propriedade');
+// alert("Valor:" + minha_propriedade);
+
 $(document).ready(function() {
     $(".click").click(function() {
         $(".click")
-            .removeClass("alert-primary"); // remove a classe de todos
+            .removeClass("alert-primary");
         $(this)
-            .addClass("alert-success"); // adiciona a classe ao botão clicado
+            .addClass("alert-success");
     });
 });
 
 $(document).ready(function() {
     $(".click2").click(function() {
         $(".click2")
-            .removeClass("alert-primary"); // remove a classe de todos
+            .removeClass("alert-primary");
         $(this)
-            .addClass("alert-danger"); // adiciona a classe ao botão clicado
+            .addClass("alert-danger");
     });
 });
